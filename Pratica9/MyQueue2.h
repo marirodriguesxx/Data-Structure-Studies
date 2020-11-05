aab7ab0c898744ba2376453ca34b726e09c4a266
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2020-11-05 18:01:36 -0300

    Finalização da Etapa 4

--- Pratica9/MyQueue2.h
+++ Pratica9/MyQueue2.h
@@ -17,33 +17,22 @@ public:
 
 	bool empty() ;
 	int size() ;
+	void change();
 
 private:
 	MyStack<T> pilha1; //nao mude nada nessa interface!!!
 	MyStack<T> pilha2; //nao mude nada nessa interface!!!
 };
 
-
 template<class T>
 void MyQueue2<T>::push(const T&el){
-	if(pilha1.empty()){
-		while(!pilha2.empty()){
-			pilha1.push(pilha2.top());
-			pilha2.pop();
-		}
-	}
-	pilha1.push(el);
+    pilha1.push(el);
 }
-template<class T>
-void MyQueue2<T>::pop(){
-	if(pilha2.empty()){
-		while(!pilha1.empty()){
-			pilha2.push(pilha1.top());
-			pilha1.pop();
-		}
-	}
-	pilha2.pop();
 
+
+template<class T>
+int MyQueue2<T>::size() {
+    return pilha1.size() + pilha2.size();
 }
 
 template<class T>
@@ -52,25 +41,29 @@ bool MyQueue2<T>::empty(){
 }
 
 template<class T>
-const T MyQueue2<T>::front(){
-	if(pilha2.empty()){
-		while(!pilha1.empty()){
-			pilha2.push(pilha1.top());
-			pilha1.pop();
-		}
+// chamamaremos essa função para copiar os elementos, em ordem inversa, da pilha 1 para a pilha 2
+void MyQueue2<T>::change(){
+	//percorremos a pilha 1 enquanto ela ainda não estiver vazia
+	//Acada iteração vamos copiando o elemento do topo da pilha 1 para a pilha 2 e, após esse processo, removemos o top da pilha 1
+	while(!pilha1.empty()){
+		pilha2.push(pilha1.top());
+		pilha1.pop();
 	}
-	return pilha2.top();
+}
+
+//Nas funçoes abaixo chamamos a change pois, quando passamos os elementos da pilha 1 para a pilha 2 fizemos com que o top correspondesse ao que seria o front de uma fila
+template<class T>
+const T MyQueue2<T>::front(){
+    if (pilha2.empty())				
+		change();
+    return pilha2.top();
 }
 
 template<class T>
-int MyQueue2<T>::size(){
-	return pilha1.size();
+void MyQueue2<T>::pop(){
+    if (pilha2.empty())
+		change();
+    pilha2.pop();
 }
-
-
-
-
-
-
 
 #endif
