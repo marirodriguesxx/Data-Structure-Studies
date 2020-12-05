ac0a0182fd63988d292f53d5f358429a9ccb96a4
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2020-12-05 17:36:57 -0300

    map3 ok

--- Trabalho3/Markov.cpp
+++ Trabalho3/Markov.cpp
@@ -33,47 +33,93 @@ void storage(const string &line, MyVec<string> &tokens){
         aux >> tok;
         // cout << "token: " << tok << endl;
         // cout<<"Tok size: "<<tok.size()<<endl;
-        if(tok.size()>0) //para nao salvarmos strings vazias em nosso
+        if(tok.size()>0) //para nao salvarmos strings vazias em
             tokens.push_back(tok);
     }
 }
 
 //Função que ordena de acordo com o valor ===========================================================================================
-void sort(MyMap<string, int> &mp1){ 
-}
+// bool smaller(const pair<string, int> &x, const pair<string, int> &y){ 
+//     return (x.second < y.second); 
+// }
+// void sort(MyMap<string, int> &map){ 
+//     MyVec<pair<string, int>> sorted;
+//     MyMapIterator<string, int> it = map.begin();
+//     while(it != Map.end()){
+//         sorted.push_back(make_pair((*it).first, (*it).second));
+//         it++;
+//     } 
+//     sort(sorted.begin(), sorted.end(), smaller());   
+  
+// }
 
 //Funções maps1, map2 e map3 auxiliares=============================================================================================
 void createmap1 (MyMap<string,int> &map1,  MyVec<string>& words){    
     for(int i=0; i<words.size(); i++){
-    if(words[i] != " " && words[i] != "\n")
         map1[words[i]]++;
     }
-
 }
-
-void createmap2(MyMap<string,MyMap<string,int>> &map2, const MyMap<string,int> &map1){
-
+void createmap2(MyMap<string,MyMap<string,int>> &map2,  MyMap<string,int> &map1, MyVec<string>& words){
+   for(int i=0; i<words.size()-1; i++){
+        map2[words[i]][words[i+1]]++;    
+    }
+}
+void createmap3(MyMap<string, MyMap<string,MyMap<string,int>>> &map3,MyMap<string,MyMap<string,int>> &map2,  
+MyMap<string,int> &map1, MyVec<string>& words){
+    for(int i=0; i<words.size()-2; i++){
+       //cout<<words[i]<<" "<<words[i+1]<<" "<<words[i+2]<<endl;
+        map3[words[i]][words[i+1]][words[i+2]]++;    
+    }
 }
 
 //Função imprime ==================================================================================================================
-void print(MyMap<string,int> &m1){
+void printMap1(MyMap<string,int> &m1){
     MyMapIterator<string, int> it1 = m1.begin();
     while(it1 != m1.end()){
         cout<<(*it1).first <<" "<<"("<< (*it1).second<<")"<<"\n";
         it1++;
     }
 }
-
-void AddDictionary( MyVec<string>& words, MyMap<string,int> &mp1){
-    createmap1(mp1,words);
-    print(mp1);
-
+void printMap2(MyMap<string,int> &m1, MyMap<string,MyMap<string,int>> &m2){
+    MyMapIterator<string,MyMap<string,int>> it2 = m2.begin();
+    while(it2 != m2.end()){
+        MyMapIterator<string, int> it1 = (*it2).second.begin();
+            while(it1 != m1.end()){
+                cout<<(*it2).first <<" "<<(*it1).first<<" "<<"("<< (*it1).second<<")"<<"\n";
+                it1++;
+            }
+            it2++;
+    }
+}
+void printMap3(MyMap<string,int> &m1, MyMap<string,MyMap<string,int>> &m2,  MyMap<string, MyMap<string,MyMap<string,int>>> &m3){
+    MyMapIterator<string, MyMap<string,MyMap<string,int>>> it3 = m3.begin();
+    while(it3 != m3.end()){
+        MyMapIterator<string,MyMap<string,int>> it2 = (*it3).second.begin();
+        while(it2 != m2.end()){
+            MyMapIterator<string, int> it1 = (*it2).second.begin();
+            while(it1 != m1.end()){
+                cout<<(*it3).first<<" "<<(*it2).first <<" "<<(*it1).first<<" "<<"("<< (*it1).second<<")"<<"\n";
+                it1++;
+            }
+            it2++;
+        }
+        it3++;
+    }
 }
 
 
+//Função para criação do dicionário ================================================================================================
+void AddDictionary( MyVec<string>& words, MyMap<string,int> &map1,
+MyMap<string,MyMap<string,int>> &map2, MyMap<string, MyMap<string,MyMap<string,int>>> &map3){
+    createmap1(map1,words);
+    createmap2(map2,map1,words);
+    createmap3(map3,map2,map1,words);
+    
+}
+
 //Funções para leitura do arquivo ==================================================================================================
-void readTreino(string &line){
-    MyVec<string> tokens;
+void readTreino(string &line, MyVec<MyVec<string>> &sentences){
+    //MyVec<MyVec<string>> sentences;
     MyMap<string,int>map1;
     MyMap<string,MyMap<string,int>>map2;
     MyMap<string, MyMap<string,MyMap<string,int>>>map3;
@@ -82,21 +128,24 @@ void readTreino(string &line){
             //Primeiro, recebemos o consteúdo das linhas e formatamos para auxiliar na criação do dicionário
             getline(cin,line);
             if(line ==  "FINAL_TREINO") break;
-            format(line);                  
+            format(line);   
+            MyVec<string> tokens;             
             storage(line,tokens);
-            //AddDictionary(tokens,mp1);
-            //cout<<tokens<<endl;
+            sentences.push_back(tokens);
+            cout<< tokens<<endl;
+            AddDictionary(tokens,map1,map2,map3);
         }
         while(line != "FINAL_TREINO");
-        AddDictionary(tokens,map1);
-        
+        //printMap1(map1);
+        //printMap2(map1,map2);
+        printMap3(map1,map2,map3);
+        //cout<<"vetor final: "<<sentences;      
     } 
 }
 
-void readComando(string &operation, string &word, int &k){
+void readComando(string &operation, string &word,char &kaux, int &k){
     // convertendo para inteiro (k) o valor recebido no arquivo
-    k = atoi(&word[1]);
-    cout<<"word[1]: "<<word[1]<<"\n";
+    k = atoi(&kaux);
     if(operation.compare("consultar") ==0)
         cout<<"estou consultando\n";
     else if (operation.compare("gerar") ==0)
@@ -106,28 +155,24 @@ void readComando(string &operation, string &word, int &k){
 
 
 int main(int argc, char *argv[]){
-    string word;
-    string line;
+    string word, operation, line;
+    char kaux;
     int k = 0;
-
+    MyVec<MyVec<string>> sentences;
     MyMap<string,int>map1;
     MyMap<string,MyMap<string,int>>map2;
     MyMap<string, MyMap<string,MyMap<string,int>>>map3;
     
 
     while(getline(cin,line)){
-        readTreino(line); 
-        
+        readTreino(line,sentences);         
         //faremos isso para ignorar a linha "FINAL_TREINO"
         // if(line != "FINAL_TREINO"){
-        // getline(cin,word);
-        // cout<<line<<word<<endl;
-        // readComando(line,word,k);
+        // cin>>operation>>kaux>>word;
+        // cout<<operation<<" "<<kaux<<" "<<word<<endl;
+        // readComando(operation,word,kaux,k);
         // cout<<"k: "<<k<<endl;
-        // }       
-        
+        // }               
     }
-    
-
     return 0;
 }
